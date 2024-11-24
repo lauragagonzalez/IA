92782a3917bffbcbd81cb89f282080d583f19923
lauragagonzalez <laura.gagonzalez@alumnos.upm.es>
2024-11-24 18:06:59 +0100

    Add files via upload

--- Datos/Distancias.py
+++ Datos/Distancias.py
@@ -10,16 +10,16 @@ DISTANCIAS = {
         ("Perú", "Plaza de Mayo", 312.92)
     ],
     "LINEA_B": [
-        ("Pasteur", "Callao", 664.36),
-        ("Callao", "Uruguay", 522.25),
+        ("Pasteur", "Callao_B", 664.36),
+        ("Callao_B", "Uruguay", 522.25),
         ("Uruguay", "Carlos Pellegrini", 491.78),
         ("Carlos Pellegrini", "Florida", 627.78),
         ("Florida", "Leandro N. Alem", 412.32)
     ],
     "LINEA_C": [
         ("Constitución", "San Juan", 616.99),
-        ("San Juan", "Independencia", 456.97),
-        ("Independencia", "Moreno", 632.36),
+        ("San Juan", "Independencia_C", 456.97),
+        ("Independencia_C", "Moreno", 632.36),
         ("Moreno", "Avenida de Mayo", 376.04),
         ("Avenida de Mayo", "Diagonal Norte", 475.74),
         ("Diagonal Norte", "Lavalle", 329.50),
@@ -27,17 +27,17 @@ DISTANCIAS = {
         ("General San Martin", "Retiro", 376.73)
     ],
     "LINEA_D":[
-        ("Facultad de Medicina", "Callao", 497.41),
-        ("Callao", "Tribunales", 749.65),
+        ("Facultad de Medicina", "Callao_D", 497.41),
+        ("Callao_D", "Tribunales", 749.65),
         ("Tribunales", "9 de Julio", 508.97),
         ("9 de Julio", "Catedral", 640.07)
     ],
     "LINEA_E":[
         ("Pichincha", "Entre Ríos", 514.80),
         ("Entre Ríos", "San José", 575.60),
-        ("San José", "Independencia", 647.70),
-        ("Independencia", "Belgrado", 624.34),
-        ("Belgrado", "Bolivar", 504.77)
+        ("San José", "Independencia_E", 647.70),
+        ("Independencia_E", "Belgrano", 624.34),
+        ("Belgrano", "Bolívar", 504.77)
     ]
 }
 
---
+++ Datos/transbordos.py
@@ -0,0 +1,11 @@
+# Agregar transbordos entre líneas
+transbordos = [
+    ("Lima", "Avenida de Mayo"),                # Línea A <-> Línea C
+    ("Perú", "Catedral"),                       # Línea A <-> Línea D
+    ("Independencia_C", "Independencia_E"),     # Línea C <-> Línea E
+    ("Bolívar", "Perú"),                        # Línea E <-> Línea A
+    ("Bolívar", "Catedral"),                    # Línea E <-> Línea D
+    ("Diagonal Norte", "Carlos Pellegrini"),    # Línea C <-> Línea B
+    ("Carlos Pellegrini", "9 de Julio"),        # Línea B <-> Línea D
+    ("9 de Julio", "Diagonal Norte"),           # Línea D <-> Línea C
+]
